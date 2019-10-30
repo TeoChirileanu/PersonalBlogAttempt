bae36bf6c93e2daba9438323cd29394eae42bfad
Teodor <teodorchirileanu@gmail.com>
2019-10-30 21:43:35 +0100

    aio

---
+++ src/Models/GetContent.cs
@@ -0,0 +1,35 @@
+﻿namespace Blog.Models
+{
+    public static class GetContent
+    {
+        public static string GetFileContent() =>
+            @"An h1 header
+============
+
+Paragraphs are separated by a blank line.
+
+2nd paragraph. *Italic*, **bold**, and `monospace`. Itemized lists
+look like:
+
+  * this one
+  * that one
+  * the other one
+
+Note that --- not considering the asterisk --- the actual text
+content starts at 4-columns in.
+
+> Block quotes are
+> written like so.
+>
+> They can span multiple paragraphs,
+> if you like.
+~~~python
+import time
+# Quick, count to ten!
+for i in range(10):
+    # (but not *too* quick)
+    time.sleep(0.5)
+    print i
+~~~";
+    }
+}
